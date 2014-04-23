cdd5840772de84e6ff357bafcc5eec98d614f2c6
ceceppa <ceceppa@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-23 16:27:58 +0000

    Cml4Woo tag 0.2
    git-svn-id: https://plugins.svn.wordpress.org/ceceppa-multilingua-support-for-woocommerce/tags/0.2@901091 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ cml4woocommerce.php
@@ -0,0 +1,114 @@
+<?php
+/*
+Plugin Name: Ceceppa Multilingua support for Woocommerce
+Plugin URI: http://www.ceceppa.eu/portfolio/ceceppa-multilingua/
+Description: Plugin to make Ceceppa Multilingua work with Woocommerce.\nThis plugin required Ceceppa Multilingua 1.4.10.
+Version: 0.1
+Author: Alessandro Senese aka Ceceppa
+Author URI: http://www.alessandrosenese.eu/
+License: GPL3
+Tags: multilingual, multi, language, admin, tinymce, qTranslate, Polyglot, bilingual, widget, switcher, professional, human, translation, service, multilingua, woocommerce
+*/
+// Make sure we don't expose any info if called directly
+if ( ! defined( 'ABSPATH' ) ) {
+	echo 'Hi there!  I\'m just a plugin, not much I can do when called directly.';
+	exit;
+}
+
+define( 'CML_WOOCOMMERCE_PATH', trailingslashit( plugin_dir_path( __FILE__ ) ) );
+define( 'CML_WOOCOMMERCE_URL', trailingslashit( plugin_dir_url( __FILE__ ) ) );
+
+require_once( CML_WOOCOMMERCE_PATH . "admin/admin.php" );
+require_once( CML_WOOCOMMERCE_PATH . "frontend/frontend.php" );
+
+class Cml4Woocommerce {
+  public function __construct() {
+    //Woocommerce post types
+    $this->_post_types = array( 'product', 'product_variation', 'shop_order', 'shop_coupon' );
+    
+    $this->_indexes = get_option( "cml_woo_indexes", array() );
+
+    add_action( 'init', array( & $this, 'rewrite_rules' ), 10 );
+  }
+
+  /*
+   * allow category slug translation in url
+   */
+  function rewrite_rules() {
+    if( ! defined( 'CECEPPA_DB_VERSION' ) ) return;
+
+    $permalinks = get_option( "cmlwoo_permalinks", array() );
+	$woo = get_option( 'woocommerce_permalinks' );
+
+    $slugs = array( 'category_base', 'tag_base' );
+    $type = array(
+                  'category_base' => 'product_cat',
+                  'tag_base' => 'product_tag',
+                 );
+
+    foreach( CMLLanguage::get_no_default() as $lang ) {
+      if( ! isset( $permalinks[ $lang->id ] ) ) continue;
+      
+      foreach( $slugs as $slug ) {
+        if( empty( $permalinks[ $lang->id ][ $slug ] ) ) continue;
+
+        $category = $permalinks[ $lang->id ][ $slug ];
+        if( $category == $woo[ $slug ] ) continue;
+  
+        add_rewrite_rule( $category . '/(.+?)/feed/(feed|rdf|rss|rss2|atom)/?$','index.php?' . $type[ $slug ] . '=$matches[1]&feed=$matches[2]', 'top' );
+        add_rewrite_rule( $category . '/(.+?)/(feed|rdf|rss|rss2|atom)/?$','index.php?' . $type[ $slug ] . '=$matches[1]&feed=$matches[2]', 'top' );
+        add_rewrite_rule( $category . '/(.+?)/page/?([0-9]{1,})/?$','index.php?' . $type[ $slug ] . '=$matches[1]&paged=$matches[2]', 'top' );
+        add_rewrite_rule( $category . '/(.+?)/?$','index.php?' . $type[ $slug ] . '=$matches[1]', 'top' );
+      }
+    }
+
+    flush_rewrite_rules();
+  }
+
+  function get_translated_title( $title, $id ) {
+    if( ! defined( 'CECEPPA_DB_VERSION' ) ) return $title;
+
+    //Get request language
+    $lang = CMLUtils::_get( "_forced_language_id", CMLLanguage::get_current_id() );
+    if( CMLLanguage::is_default( $lang ) ) return $title;
+
+    //In the loop in can't use is_singular so I check if current $id exists
+    //in "woocommerce" post types
+    if( ! in_array( $id, $this->_indexes ) ) return $title;
+
+    $meta = $this->get_meta( $id );
+    if( empty( $meta ) ) return $title;
+
+    $c = isset( $meta[ 'title' ] ) ? $meta[ 'title' ] : "";
+    if( !empty( $c ) ) $title = $c;
+
+    return $title;
+  }
+  
+
+  function get_meta( $id = null ) {
+    if( null == $id ) {
+      $id = get_the_ID();
+    }
+
+    $lang = CMLUtils::_get( "_forced_language_id", CMLLanguage::get_current_id() );
+
+    if( ! isset( $this->_metas[ $id ][ $lang ] ) ) {
+      $meta = get_post_meta( $id, "_cml_woo_" . $lang, true );
+
+      $this->_metas[ $id ][ $lang ] = $meta;
+    } else {
+      $meta = $this->_metas[ $id ][ $lang ];
+    }
+
+    return $meta;
+  }
+
+}
+
+if( is_admin() ) {
+	$cml4woocommerce = new Cml4WoocommerceAdmin();
+} else {
+	$cml4woocommerce = new Cml4WoocommerceFrontend();
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,56 @@
+=== Ceceppa Multilingua support to WooCommerce ===
+Contributors: ceceppa
+Tags: multilingual, language, admin, tinymce, bilingual, widget, switcher, i18n, l10n, multilanguage, professional, translation, service, human, qtranslate, wpml, ztranslate, xtranslate, international, .mo file, .po file, localization, widget, post, woocommerce
+Requires at least: 3.4.1
+Tested up to: 3.9
+Stable tag: 0.1
+License: GPLv3 or later
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+Allows running fully multilingual e-commerce sites using WooCommerce and Ceceppa Multilingua.
+
+== Description ==
+
+This plugin makes it possible to run fully multilingual e-commerce sites using WooCommerce and Ceceppa Multilingua.
+It makes products and store pages translatable, lets visitors switch languages and order products in their language.
+
+= Features =
+
+- Translating product categories, tags and custom attributes
+- Translating media “Title” and “Alternative Text” for “Featured Image” and “Product Gallery”
+- Keeps the same language through the checkout process
+- Translate category and tag slug
+
+= Requires =
+
+Ceceppa Multilingua >= 1.4.17
+
+= About =
+For more Information visit the [Plugin Homepage](http://www.alessandrosenese.eu/en/ceceppa-multilingua/ceceppa-multilingua-for-woocommerce)
+
+== Installation ==
+
+Installation of this plugin is fairly easy:
+
+1. Download the plugin from wordpress. 
+1. Extract all the files. 
+1. Upload everything (keeping the directory structure) to the `/wp-content/plugins/` directory.
+1. Activate the plugin through the 'Plugins' menu in WordPress.
+1. Manage desidered languages
+
+== Frequently Asked Questions ==
+
+== Screenshots ==
+
+1. Product title translation
+2. Product short description translation
+
+== Changelog ==
+
+= 0.2 =
+* Allow permalink customization for each language
+* Added option to disable product permalink translation
+* Added error when Product category or tag base is empty
+
+= 0.1 =
+* First release
